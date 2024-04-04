f71011c27de37508f59ab0f84c2f1d0cccd5fb73
Martin de Larminat <mcdelarm@email.unc.edu>
2024-04-04 18:58:47 +0100

    Add files via upload

---
+++ studybud/base/api/serializers.py
@@ -0,0 +1,13 @@
+#Used to convert objects to JSON format
+from rest_framework.serializers import ModelSerializer
+from base.models import Room, Topic
+
+class RoomSerializer(ModelSerializer):
+  class Meta:
+    model = Room
+    fields = '__all__'
+
+class TopicSerializer(ModelSerializer):
+  class Meta:
+    model = Topic
+    fields = '__all__'
